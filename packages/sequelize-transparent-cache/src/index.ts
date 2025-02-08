4a58bffcd71ca4478630cbacb7a51eec1d5b14eb
Chris <38118010+Kaimbelic@users.noreply.github.com>
2025-02-08 13:19:52 -0500

    Adding basic type for high level cache design

--- packages/sequelize-transparent-cache/src/index.ts
+++ packages/sequelize-transparent-cache/src/index.ts
@@ -1,14 +1,14 @@
 import { auto as buildClassAutoMethods, manual as buildClassManualMethods } from './methods/class';
 import buildInstanceMethods from './methods/instance';
 import { Model, ModelStatic } from 'sequelize';
-import { CacheableModelClass } from './types';
+import { CacheableModelClass, CacheClient } from './types';
 
 /**
  * Initializes the cache client and returns an object with the withCache method.
  * @param client - The cache client.
  * @returns An object containing the withCache method.
  */
-export default (client: any) => ({
+export default (client: CacheClient) => ({
   /**
    * Adds caching methods to a Sequelize model class.
    * @param modelClass - The Sequelize model class.
--- packages/sequelize-transparent-cache/src/types.ts
+++ packages/sequelize-transparent-cache/src/types.ts
@@ -10,3 +10,10 @@ export interface CacheableModelClass extends ModelStatic<Model<any, any>> {
 export type CacheableModelInstance<T extends Model<any, any>> = T & {
   cache: () => any;
 };
+
+// Define a type for the cache clients
+export type CacheClient = {
+  set: (key: string[], value: any) => Promise<any>;
+  get: (key: string[]) => Promise<any>;
+  del: (key: string[]) => Promise<any>;
+};
